9e457133f568d790183e978abe3282ba7d1cea85
syang <steven.s.yang@gmail.com>
2018-05-18 19:57:47 -0700

    debug affine_layer()

--- assignment/a1/tensorflow/graph.py
+++ assignment/a1/tensorflow/graph.py
@@ -30,6 +30,7 @@ def affine_layer(hidden_dim, x):
     Hint: always initialize "b" as 0s.  It isn't a constant though!
           It needs to be a trainable variable!
     '''
+    tf.set_random_seed(0)
     
     dim_features = x.shape[-1].value
     batch_size = x.shape[0].value
