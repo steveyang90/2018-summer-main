c9d07e3e09cf0abc99fc7f1a682844bf7963bd4f
syang <steven.s.yang@gmail.com>
2018-07-04 12:25:17 -0700

    WIP a4 implement BuildTrainGraph() debug

--- assignment/a4/rnnlm.py
+++ assignment/a4/rnnlm.py
@@ -269,7 +269,7 @@ class RNNLM(object):
             train_individual_loss_ = tf.nn.sampled_softmax_loss(
                 weights = tf.transpose(self.W_out_),
                 biases = self.b_out_,
-                labels = tf.reshape(self.target_y_, [-1]),
+                labels = tf.reshape(self.target_y_, [-1, 1]),
                 inputs = tf.reshape(self.o_, [-1, self.H]),
                 num_sampled = self.softmax_ns
             )
